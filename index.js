104d0910b3511b843cddc66bdfa65d7cf0b69d6a
Yuze Wang <yuzewang621@gmail.com>
2023-05-05 06:25:07 -0600

    function that sums all the numbers in an array using recursion

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let num = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // sum the result of sumItems to num
+      num += sumItems(item);
+    } else {
+      // sum up item to num
+      num += item;
+    }
+  });
+  return num;
+};
+
+//Testcases
+const array = [[1, 2, [[3], 4]], 5, [6],[]];
+const array1 = [[[[[[[[[[[[[1]]]]]]]]]]]]];
+console.log(sumItems(array));
+console.log(sumItems(array1));
 
 module.exports = sumItems;
